3da1b9015dde829af5f5d24a5fa9d4bd85790185
Vincent Smeets <Vincent.VSmeets@GMail.com>
2022-05-30 19:09:45 +0200

    7: Dependencies within an umbrella project

--- apps/kv/mix.exs
+++ apps/kv/mix.exs
@@ -1,15 +1,15 @@
-defmodule KVServer.MixProject do
+defmodule KV.MixProject do
   use Mix.Project
 
   def project do
     [
-      app: :kv_server,
+      app: :kv,
       version: "0.1.0",
       build_path: "../../_build",
       config_path: "../../config/config.exs",
       deps_path: "../../deps",
       lockfile: "../../mix.lock",
-      elixir: "~> 1.12",
+      elixir: "~> 1.10",
       start_permanent: Mix.env() == :prod,
       deps: deps()
     ]
@@ -19,12 +19,15 @@ defmodule KVServer.MixProject do
   def application do
     [
       extra_applications: [:logger],
-      mod: {KVServer.Application, []}
+      mod: {KV, []}
     ]
   end
 
   # Run "mix help deps" to learn about dependencies.
   defp deps do
-    [{:kv, in_umbrella: true}]
+    [
+      # {:dep_from_hexpm, "~> 0.3.0"},
+      # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
+    ]
   end
 end
