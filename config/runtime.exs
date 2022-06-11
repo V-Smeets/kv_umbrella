776525475beecfd64c46929be4c0fdd63157c618
Vincent Smeets <Vincent.VSmeets@GMail.com>
2022-06-11 21:21:59 +0200

    11: Releases

--- config/runtime.exs
+++ config/runtime.exs
@@ -1,2 +1,12 @@
 import Config
+
 config :kv, :routing_table, [{?a..?z, node()}]
+
+{:ok, hostname} = :inet.gethostname()
+
+if config_env() == :prod do
+  config :kv, :routing_table, [
+    {?a..?m, :"foo@#{hostname}"},
+    {?n..?z, :"bar@#{hostname}"}
+  ]
+end
